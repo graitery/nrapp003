01b7c3142f342f4f9da957a88d7970aea73556ae
mid <mid>
2017-03-23 22:45:48 +0100

    ouais ouais ouais

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('newrelic');
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<html><head><title>Hello!</title></head><body><h1>Hello World!</h1></body></html>');
 })
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
-})
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
